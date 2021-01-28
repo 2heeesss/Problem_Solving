23e79aa30dd09e6be0ae6b6f4fef88c9ae303b57
2heeesss <hohooodo@gmail.com>
2021-01-28 23:44:52 +0900

    Add DFS practice

---
+++ BOJ/DFS_BFS/1260.py
@@ -0,0 +1,24 @@
+n, m, v = map(int, input().split())
+
+graph = []
+graphList = []
+graph.append
+for i in range(m):
+    graph.append(list(map(int, input().split())))
+
+for i in range(m):
+    for j in range(2):
+        if graph[i][j]:
+            pass
+
+# print(graph[1][0])
+
+
+# def dfs(graph, v, visted):
+#     visted[v] = True
+#     print(v, end='')
+#     for i in graph[v]:
+#         if not visted[]
+
+
+# end 까먹었지? 기억하고있자
