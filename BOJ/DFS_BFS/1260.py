296cf3f59c42b91e99c66b9c5fbc5b54b217923b
2heeesss <hohooodo@gmail.com>
2021-01-29 23:55:26 +0900

    Update DFS part

--- BOJ/DFS_BFS/1260.py
+++ BOJ/DFS_BFS/1260.py
@@ -1,24 +1,42 @@
+from collections import deque
+
+
+def dfs(graph, v, visted):
+    visted[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visted[i]:
+            visted[i] = True
+            dfs(graph, i, visted)
+
+
+def bfs(graph, start, visted):
+    queue = deque([start])
+    visted[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visted[i]:
+                queue.append(i)
+                visted[i] = True
+
+
 n, m, v = map(int, input().split())
 
-graph = []
-graphList = []
-graph.append
+graph = [[0 for j in range(0)] for i in range(n+1)]
 for i in range(m):
-    graph.append(list(map(int, input().split())))
-
-for i in range(m):
-    for j in range(2):
-        if graph[i][j]:
-            pass
-
-# print(graph[1][0])
+    edge_front, edge_back = map(int, input().split())
+    graph[edge_front].append(edge_back)
+    graph[edge_back].append(edge_front)
+# 그래프 정렬하기
+for node in graph:
+    node.sort()
 
 
-# def dfs(graph, v, visted):
-#     visted[v] = True
-#     print(v, end='')
-#     for i in graph[v]:
-#         if not visted[]
+visted_dfs = [False]*(n+1)
+visted_bfs = [False]*(n+1)
 
-
-# end 까먹었지? 기억하고있자
+dfs(graph, v, visted_dfs)
+print()
+bfs(graph, v, visted_bfs)
