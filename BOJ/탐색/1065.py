74a7d528e61d5d1167623868a2d4fbd9b2fabc68
2heeesss <hohooodo@gmail.com>
2021-04-07 10:58:59 +0900

    Add 한수 #브루트포스

---
+++ BOJ/탐색/1065.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def isHanNum(one, ten, hun):
+    first = ten - one
+    second = hun - ten
+    if first == second:
+        return 1
+    return 0
+
+
+def d(number):
+    if 1 <= number <= 99:
+        return 1
+    elif 100 <= number <= 999:
+        ones = number % 10
+        tens = (number // 10) % 10
+        huns = (number // 10) // 10
+        return isHanNum(ones, tens, huns)
+    else:
+        return 0
+
+
+input = sys.stdin.readline
+n = int(input().rstrip())
+cnt = 0
+result = []
+
+while n != cnt:
+    result.append(d(cnt+1))
+    cnt += 1
+
+print(sum(result))
+
+# print(10-5, 5-10)
+
+# if (10-5) == (5-10):
+#     print(123)
