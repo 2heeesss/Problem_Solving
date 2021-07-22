d3485fe6d86206fbf2e5f61f8eb0f6aa0b6bb4f2
2heeesss <hohooodo@gmail.com>
2021-07-22 15:41:23 +0900

    Improve time complexity in last print

--- reBOJ/5430.py
+++ reBOJ/5430.py
@@ -32,7 +32,7 @@ for _ in range(int(input())):
 
     if result:
         numsFinal = list(nums)
-        if func.count('R') % 2 == 0:
+        if leftPop:
             print('['+','.join(map(str, numsFinal))+']')
         else:
             print('['+','.join(map(str, numsFinal[::-1]))+']')
@@ -43,6 +43,8 @@ for _ in range(int(input())):
 # list.count('문자') 사용방법 처음씀
 # 1차시도 ValueError
 # 배열로 들어오는 숫자가 2자리수 이상이면 잘 안됨
+# 리스트 뒤집는법
+# 리스트 슬라이싱 [1:-1]
 
 # def newList(array, length):
 #     n = []
